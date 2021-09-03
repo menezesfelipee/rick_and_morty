e29015f44cb81b98a309c7ef94a3d0bcc3c455c5
menezesfelipee <sa.felipem@gmail.com>
2021-09-03 11:20:15 -0300

    Pequenos ajustes

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-const express = require("express");
-const { MongoClient } = require("mongodb");
-const { ObjectId } = require("mongodb");
 require("dotenv").config();
 
+const express = require("express");
+const { MongoClient, ObjectId } = require("mongodb");
+
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbChar = process.env.DB_CHAR;
+const dbName = process.env.DB_NAME;
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.json());
+
+// const uri = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.${dbChar}.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+const uri = "mongodb://localhost:27017";
+const client = new MongoClient(uri, { useUnifiedTopology: true });
+
 (async () => {
-    const dbUser = process.env.DB_USER;
-    const dbPassword = process.env.DB_PASSWORD;
-    const dbChar = process.env.DB_CHAR;
-    const dbName = process.env.DB_NAME;
+    const getPersonagensValidas = () => personagens.find({}).toArray();
+    const getPersonagemById = async (id) => personagens.findOne({ _id: ObjectId(id) });
 
-    const app = express();
-    const port = process.env.PORT || 3000;
-    app.use(express.json());
-
-    // const uri = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.${dbChar}.mongodb.net/${dbName}?retryWrites=true&w=majority`;
-    const uri = "mongodb://localhost:27017/rick-and-morty"
-    const options = { useNewUrlParser: true, useUnifiedTopology: true }
-    const client = await MongoClient.connect(uri, options);
+    await client.connect();
     
     const db = client.db(dbName);
     const personagens = db.collection("personagens");
 
-    const getPersonagensValidas = () => personagens.find({}).toArray();
-	const getPersonagemById = async (id) => personagens.findOne({ _id: ObjectId(id) });
 
     app.all("/*", (req, res, next) => {
 		res.header("Access-Control-Allow-Origin", "*");
@@ -45,8 +47,6 @@ require("dotenv").config();
         res.send(await getPersonagensValidas());
     });
 
-    app.use(express.json());
     app.listen(port, () => console.log(`Aplicativo rodando em http://localhost:${port}.`));
-
 
 })();
